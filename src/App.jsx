4cbe92974a709c4bd3a1674cfa4ca3f32d495486
Govind Harsh Sharma <govindharsh42@gmail.com>
2023-06-15 16:27:38 +0530

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/header/header";
+import Hero from "./components/hero/hero";
+import "./App.css";
+import Companies from "./components/companies/companies";
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+        <Header/><Hero/>
+      </div>
+         <Companies/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+    return (
+        <section className="h-wrapper">
+            <div className="flexCenter paddings innerWidth h-container">
+                <img src="./logonew.png" alt="logo" width={200} />
+
+                <div className="flexCenter h-menu">
+                    <a href="">residences</a>
+                    <a href="">values</a>
+                    <a href="">contact</a>
+                    <a href="">get started</a>
+                    <button className="button">
+                        <a href="">Contact</a>
+                    </button>
+
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Header;
